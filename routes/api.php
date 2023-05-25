931ca550ebc5f496945ff8a8862d93282abfa28c
Elivelton Cotrim <elivelton.gbi@gmail.com>
2023-05-25 12:05:42 -0300

    .

--- routes/api.php
+++ routes/api.php
@@ -20,12 +20,6 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-// Route::apiResource('/service-orders', ServiceOrderController::class);
 Route::apiResource('/service-orders', ServiceOrderController::class);
 Route::apiResource('/users', UserController::class);
 
-Route::get('/test-api', function(){
-    return response()->json([
-        'message' => 'Hello World!'
-    ]);
-});
